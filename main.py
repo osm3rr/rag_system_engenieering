8d825b9812e7420ef352efe5cbd77bfa17440732
parabavire <jdparabavire@gmail.com>
2025-09-10 00:08:18 -0400

    segundo commit

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pydantic import BaseModel
 from langgraph.graph import StateGraph, END, START
 from langchain_core.messages import SystemMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.types import interrupt
 
 # Carga variable de entorno
 load_dotenv()
@@ -14,6 +15,19 @@ load_dotenv()
 from agent.state import EstadoGlobal
 from agent.supervisor import DecisionSupervisor
 from agent.specialists import agente_consultas, agente_inscripcion, agente_pagos
+
+# --- NODO ENTRADA HUMANA ---
+# Este nodo simplemente espera la siguiente entrada del usuario
+def entrada_humana(state: EstadoGlobal):
+    """Pausa la ejecución para esperar la siguiente entrada del usuario."""
+    
+    # Pausamos el grafo y enviamos una señal al cliente indicando que estamos listos.
+    # Este string "Ready for user input." será el valor que recibirá el cliente en la interrupción.
+    respuesta_usuario = interrupt("Ready for user input.")
+    
+    # Cuando la ejecución se reanude, 'respuesta_usuario' contendrá la entrada del cliente.
+    # El valor se agrega al historial de mensajes para que el supervisor lo vea.
+    return {"messages": [("user", respuesta_usuario)]}
 
 # --- NODO SUPERVISOR ---
 def supervisor_node(state: EstadoGlobal) -> dict:
@@ -49,9 +63,10 @@ builder.add_node("supervisor", supervisor_node)
 builder.add_node("agente_consultas", agente_consultas)
 builder.add_node("agente_inscripcion", agente_inscripcion)
 builder.add_node("agente_pagos", agente_pagos)
+builder.add_node("entrada_humana", entrada_humana)
 
 # El punto de entrada es siempre el supervisor
-builder.set_entry_point("supervisor")
+builder.add_edge(START, "supervisor")
 
 # Aristas condicionales: desde el supervisor hacia los especialistas
 builder.add_conditional_edges(
@@ -66,9 +81,12 @@ builder.add_conditional_edges(
 )
 
 # Aristas de retorno: después de que un especialista termina, vuelve al supervisor
-builder.add_edge("agente_consultas", END)
-builder.add_edge("agente_inscripcion", END)
-builder.add_edge("agente_pagos", END)
+builder.add_edge("agente_consultas", "entrada_humana")
+builder.add_edge("agente_inscripcion", "entrada_humana")
+builder.add_edge("agente_pagos", "entrada_humana")
+
+# Después de recibir la entrada humana, volvemos al supervisor para que decida
+builder.add_edge("entrada_humana", "supervisor")
 
 # Compilamos el grafo final
 workflow_supervisor = builder.compile()
